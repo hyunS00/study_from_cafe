1a180b1031227e9fe3bf5743f43e351c85add061
hyunS00 <optifun72@gmail.com>
2023-06-08 14:43:03 +0900

    mysql 연결 에러..

---
+++ src/main/java/com/example/study_from_cafe/StudyFromCafeApplication.java
@@ -0,0 +1,13 @@
+package com.example.study_from_cafe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyFromCafeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudyFromCafeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/study_from_cafe/StudyFromCafeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.study_from_cafe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyFromCafeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
